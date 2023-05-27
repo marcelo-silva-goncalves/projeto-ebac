10c07f933b31bad8178c5a2ab36df2492c55ada5
seu-usuario-git <marcelodfin@gmail.com>
2023-05-27 09:55:32 +0000

    proj. EBAC-Analista de Dados-exercicio 20 - gasolina.csv dataset para elaboracao de grafico - alteracao no valor do dia 07/07

--- price_gas.py
+++ price_gas.py
@@ -7,13 +7,14 @@ import pandas as pd
 price_gas = pd.read_csv('gasolina.csv', sep=',') 
 price_gas.head()
 
+
 #Visualização gráfica dos dados e salvando imagem em png
 with sns.axes_style('darkgrid'):
-  plt.figure(figsize=(12, 6)) # Altere a largura e a altura para ajustar o tamanho do gráfico
+  plt.figure(figsize=(9, 5)) # Altere a largura e a altura para ajustar o tamanho do gráfico
   grafico_price_gas = sns.lineplot(data=price_gas, x='dia', y='venda', palette='pastel')
-  grafico_price_gas.set(title='Preço da Gasolina na cidade de São Paulo no 1º decênio de julho/2021', xlabel='dia', ylabel='preço')
+  grafico_price_gas.set(title='Preço da Gasolina, de 1 a 10/07/2021, na cidade de São Paulo', xlabel='dia', ylabel='preço')
   grafico_price_gas.grid(False) #retirando o grid visando deixar melhor a visualização
-  grafico_price_gas.set_title('Preço da gasolina no 1º decêndio de julho/2021 em São Paulo', fontsize=17)
+  grafico_price_gas.set_title('Preço da Gasolina, de 1 a 10/07/2021, na cidade de São Paulo', fontsize=15)
   grafico_price_gas.set_xlabel('Dia', fontsize=14) 
   grafico_price_gas.set_ylabel('Preço', fontsize=14)
   plt.savefig('grafico_gasolina.png')
